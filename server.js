ff657835551625034fbcb36e8b6279cc20002923
noirvin <vin.sp97@googlemail.com>
2020-07-20 07:14:58 -0400

    reddit init

---
+++ server.js
@@ -0,0 +1,25 @@
+var path = require('path');
+const express = require('express')
+const app = express()
+var cookieParser = require('cookie-parser');
+// require handlebars
+const exphbs = require('express-handlebars');
+
+// Use "main" as our default layout
+app.set('views', path.join(__dirname, 'views/layouts'));
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: false }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('main');
+})
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
